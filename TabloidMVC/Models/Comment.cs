c47841ad0998c1f62fe42dcc064274a8a660722d
ste163 <samtedwards@pm.me>
2021-01-05 11:54:26 -0500

    added comment model and repo interface

---
+++ TabloidMVC/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TabloidMVC.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime CreateDataTime { get; set; }
+    }
+}
---
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -0,0 +1,10 @@
+﻿using TabloidMVC.Models;
+using System.Collections.Generic;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ICommentRepository
+    {
+        List<Comment> getAllCommentsForPost(int id);
+    }
+}
